55848146dc229f2a5a78f9f6d28e50d308df8cb7
unknown <andrewwaugaman@arizona.edu>
2025-05-03 19:16:27 -0700

    added a better test

--- dbImputer.py
+++ dbImputer.py
@@ -172,11 +172,11 @@ def doImputation():
             for i in [0.00001, 0.0001, 0.001]:
                 print("Attempting SGD...")
                 sgd = sklearn.linear_model.SGDRegressor(max_iter=1000, alpha=i, learning_rate='invscaling')
-                sgd.fit(preppedFeatures, preppedLabels)
                 cvals = sklearn.model_selection.cross_val_score(sgd, preppedFeatures, preppedLabels)
                 cvalAvg = np.mean(cvals)
                 if cvalAvg > bestCvalAvg:
                     bestCvalAvg = cvalAvg
+                    sgd.fit(preppedFeatures, preppedLabels)
                     bestModel = sgd
                     bestCvals = cvals
             print("Cross Val Scores: " + str(bestCvals) + "\nAverage: " + str(bestCvalAvg) + "\n")
@@ -188,10 +188,10 @@ def doImputation():
             if fewFeatures:
                 print("Attempting LASSO...")
                 lassoReg = sklearn.linear_model.LassoCV(cv=5)
-                lassoReg.fit(preppedFeatures, preppedLabels)
                 cvals = sklearn.model_selection.cross_val_score(lassoReg, preppedFeatures, preppedLabels)
                 bestCvalAvg = np.mean(cvals)
                 bestCvals = cvals
+                lassoReg.fit(preppedFeatures, preppedLabels)
                 bestModel = lassoReg
                 print("Cross Val Scores: " + str(bestCvals) + "\nAverage: " + str(bestCvalAvg) + "\n")
                 impute = input("Is this acceptable for this data?\n1. Yes\nOther. No\n") == "1"
@@ -201,11 +201,11 @@ def doImputation():
                 
                 print("Attempting Elastic Net...")
                 elasticNet = sklearn.linear_model.ElasticNetCV(l1_ratio=0.7, cv=5)
-                elasticNet.fit(preppedFeatures, preppedLabels)
                 cvals = sklearn.model_selection.cross_val_score(elasticNet, preppedFeatures, preppedLabels)
                 if np.mean(cvals) > bestCvalAvg:
                     bestCvals = cvals
                     bestCvalAvg = np.mean(cvals)
+                    elasticNet.fit(preppedFeatures, preppedLabels)
                     bestModel = elasticNet
                     print("Cross Val Scores: " + str(bestCvals) + "\nAverage: " + str(bestCvalAvg) + "\n")
                     impute = input("Is this acceptable for this data?\n1. Yes\nOther. No\n") == "1"
@@ -215,10 +215,10 @@ def doImputation():
             else:
                 print("Attempting Ridge Regression...")
                 ridgeReg = sklearn.linear_model.RidgeCV(alphas=[0.1, 1.0, 10.0], cv=5)
-                ridgeReg.fit(preppedFeatures, preppedLabels)
                 cvals = sklearn.model_selection.cross_val_score(ridgeReg, preppedFeatures, preppedLabels)
                 bestCvals = cvals
                 bestCvalAvg = np.mean(cvals)
+                ridgeReg.fit(preppedFeatures, preppedLabels)
                 bestModel = ridgeReg
                 print("Cross Val Scores: " + str(bestCvals) + "\nAverage: " + str(bestCvalAvg) + "\n")
                 impute = input("Is this acceptable for this data?\n1. Yes\nOther. No\n") == "1"
@@ -229,11 +229,11 @@ def doImputation():
                 print("Attempting Linear SVR...")
                 for i in [0.1, 1, 10]:
                     svrLinear = sklearn.svm.SVR(kernel='linear', C=i, max_iter=10000)
-                    svrLinear.fit(preppedFeatures, preppedLabels)
                     cvals = sklearn.model_selection.cross_val_score(svrLinear, preppedFeatures, preppedLabels)
                     cvalAvg = np.mean(cvals)
                     if cvalAvg > bestCvalAvg:
                         bestCvalAvg = cvalAvg
+                        svrLinear.fit(preppedFeatures, preppedLabels)
                         bestModel = svrLinear
                         bestCvals = cvals
                         bestModelChanged = True
@@ -248,11 +248,11 @@ def doImputation():
                 print("Attempting RBF SVR...")
                 for i in [0.1, 1, 10]:
                     svrRbf = sklearn.svm.SVR(kernel='rbf', C=i, max_iter=10000)
-                    svrRbf.fit(preppedFeatures, preppedLabels)
                     cvals = sklearn.model_selection.cross_val_score(svrRbf, preppedFeatures, preppedLabels)
                     cvalAvg = np.mean(cvals)
                     if cvalAvg > bestCvalAvg:
                         bestCvalAvg = cvalAvg
+                        svrRbf.fit(preppedFeatures, preppedLabels)
                         bestModel = svrRbf
                         bestCvals = cvals
                         bestModelChanged = True
@@ -276,11 +276,11 @@ def doImputation():
             print("Attempting SGD classifier...")
             for i in [0.00001, 0.0001, 0.001]:
                 sgdClass = sklearn.linear_model.SGDClassifier(max_iter=1000, alpha=i, learning_rate='invscaling')
-                sgdClass.fit(preppedFeatures, preppedLabels)
                 cvals = sklearn.model_selection.cross_val_score(sgdClass, preppedFeatures, preppedLabels)
                 cvalAvg = np.mean(cvals)
                 if cvalAvg > bestCvalAvg:
                     bestCvalAvg = cvalAvg
+                    sgdClass.fit(preppedFeatures, preppedLabels)
                     bestModel = sgdClass
                     bestCvals = cvals
             print("Cross Val Scores: " + str(bestCvals) + "\nAverage: " + str(bestCvalAvg) + "\n")
@@ -292,11 +292,11 @@ def doImputation():
             print("Attempting kernel classifier...")
             for i in [0.1, 1, 10]:
                 kernel = sklearn.kernel_approximation.RBFSampler(gamma=i, random_state=1)
-                kernel.fit(preppedFeatures, preppedLabels)
                 cvals = sklearn.model_selection.cross_val_score(kernel, preppedFeatures, preppedLabels)
                 cvalAvg = np.mean(cvals)
                 if cvalAvg > bestCvalAvg:
                     bestCvalAvg = cvalAvg
+                    kernel.fit(preppedFeatures, preppedLabels)
                     bestModel = kernel
                     bestCvals = cvals
                     bestModelChanged = True
@@ -310,11 +310,11 @@ def doImputation():
             print("Attempting Linear SVC classifier...")
             for i in [0.1, 1, 10]:
                 linearSVC = sklearn.svm.LinearSVC(C=i)
-                linearSVC.fit(preppedFeatures, preppedLabels)
                 cvals = sklearn.model_selection.cross_val_score(linearSVC, preppedFeatures, preppedLabels)
                 cvalAvg = np.mean(cvals)
                 if cvalAvg > bestCvalAvg:
                     bestCvalAvg = cvalAvg
+                    linearSVC.fit(preppedFeatures, preppedLabels)
                     bestModel = linearSVC
                     bestCvals = cvals
                     bestModelChanged = True
@@ -329,11 +329,11 @@ def doImputation():
             print("Attempting KNN classifier...")
             for i in [4, 8, 16]:
                 knn = sklearn.neighbors.KNeighborsClassifier(n_neighbors=i)
-                knn.fit(preppedFeatures, preppedLabels)
                 cvals = sklearn.model_selection.cross_val_score(knn, preppedFeatures, preppedLabels)
                 cvalAvg = np.mean(cvals)
                 if cvalAvg > bestCvalAvg:
                     bestCvalAvg = cvalAvg
+                    knn.fit(preppedFeatures, preppedLabels)
                     bestModel = knn
                     bestCvals = cvals
                     bestModelChanged = True
@@ -348,11 +348,11 @@ def doImputation():
             print("Attempting SVC classifier...")
             for i in [0.1, 1, 10]:
                 svc = sklearn.svm.SVC(C=i, max_iter=10000)
-                svc.fit(preppedFeatures, preppedLabels)
                 cvals = sklearn.model_selection.cross_val_score(svc, preppedFeatures, preppedLabels)
                 cvalAvg = np.mean(cvals)
                 if cvalAvg > bestCvalAvg:
                     bestCvalAvg = cvalAvg
+                    svc.fit(preppedFeatures, preppedLabels)
                     bestModel = svc
                     bestCvals = cvals
                     bestModelChanged = True
@@ -487,6 +487,234 @@ def csvToTableUser():
     csvToTable(tableName, filePath)
     return
 
+def runTest():
+    colName = 'target'
+    listOfTables = cursor.execute("SELECT tbl_name FROM sqlite_master WHERE type='table'").fetchall()
+    for table in listOfTables:
+        cursor.execute("DROP TABLE \'" + table[0] + "\'")
+        conn.commit()
+    tableName = ""
+    userInput = input("Options:\n" + 
+                      "1. Iris Dataset\n" + 
+                      "2. Diabetes\n" + 
+                      "3. Digits\n" + 
+                      "4. Wine\n" + 
+                      "5. Breast Cancer\n")
+    if userInput == "1":
+        tableName = "Iris"
+        sklearnToTable(0)
+    elif userInput == "2":
+        tableName = "Diabetes"
+        sklearnToTable(1)
+    elif userInput == "3":
+        tableName = "Digits"
+        sklearnToTable(2)
+    elif userInput == "4":
+        tableName = "Wine"
+        sklearnToTable(3)
+    elif userInput == "5":
+        tableName = "Breast Cancer"
+        sklearnToTable(4)
+    else:
+        print("Option not recognized.")
+        return
+    
+    listOfColsTups = cursor.execute("select name from pragma_table_info('" + tableName + "') as tblInfo;").fetchall()
+    listOfCols = []
+    for col in listOfColsTups:
+        listOfCols.append(col[0])
+
+    table = pandas.read_sql_query(sql="SELECT * FROM \'" + tableName + "\';", con=conn, coerce_float=True)
+    for idx, col in enumerate(listOfCols):
+        table.rename(index={idx: col})
+
+    gen = np.random.default_rng()
+    table[colName] = table.apply(lambda x: x[colName] if gen.random() < 0.9 else np.nan, axis=1)
+
+    table.to_sql(name=tableName, if_exists="replace", con=conn, index=False)
+    conn.commit()
+    
+    otherCols = table.columns.to_list()
+    otherCols.remove(colName)
+    notNullData = table.dropna(axis=0, subset=[colName])
+    if len(notNullData) == len(table):
+        print("Something went wrong, there are no missing values to imputate.")
+        return
+    preppedFeatures, preppedNullFeatures = prepData(table, colName)
+    preppedLabels = notNullData[colName].to_numpy()
+
+    bestModel = None
+    bestModelName = ""
+    bestCvalAvg = 0
+    bestCvals: np.ndarray
+    bestCurCvals: np.ndarray
+
+    classFlag = (userInput != "2")
+
+    if not classFlag: #Regression
+
+        print("\nBeginning model selection.\n")
+
+        print("Attempting LASSO...")
+        lassoReg = sklearn.linear_model.LassoCV(cv=5)
+        cvals = sklearn.model_selection.cross_val_score(lassoReg, preppedFeatures, preppedLabels)
+        bestCvalAvg = np.mean(cvals)
+        bestCvals = cvals
+        lassoReg.fit(preppedFeatures, preppedLabels)
+        bestModel = lassoReg
+        bestModelName = "LASSO"
+        print("Cross Val Scores: " + str(bestCvals) + "\nAverage: " + str(bestCvalAvg) + "\n")
+        
+        bestCurCvalAvg = 0
+
+        print("Attempting Elastic Net...")
+        elasticNet = sklearn.linear_model.ElasticNetCV(l1_ratio=0.7, cv=5)
+        cvals = sklearn.model_selection.cross_val_score(elasticNet, preppedFeatures, preppedLabels)
+        if np.mean(cvals) > bestCvalAvg:
+            bestCvals = cvals
+            bestCvalAvg = np.mean(cvals)
+            elasticNet.fit(preppedFeatures, preppedLabels)
+            bestModel = elasticNet
+            bestModelName = "Elastic Net"
+        bestCurCvalAvg = np.mean(cvals)
+        bestCurCvals = cvals
+        print("Cross Val Scores: " + str(bestCurCvals) + "\nAverage: " + str(bestCurCvalAvg) + "\n")
+        
+        bestCurCvalAvg = 0
+
+        print("Attempting Ridge Regression...")
+        ridgeReg = sklearn.linear_model.RidgeCV(alphas=[0.1, 1.0, 10.0], cv=5)
+        cvals = sklearn.model_selection.cross_val_score(ridgeReg, preppedFeatures, preppedLabels)
+        if np.mean(cvals) > bestCvalAvg:
+            bestCvals = cvals
+            bestCvalAvg = np.mean(cvals)
+            ridgeReg.fit(preppedFeatures, preppedLabels)
+            bestModel = ridgeReg
+            bestModelName = "Ridge Regression"
+        if np.mean(cvals) > bestCurCvalAvg:
+            bestCurCvals = cvals
+            bestCurCvalAvg = np.mean(cvals)
+        print("Cross Val Scores: " + str(bestCurCvals) + "\nAverage: " + str(bestCurCvalAvg) + "\n")
+        
+        bestCurCvalAvg = 0
+
+        print("Attempting Linear SVR...")
+        for i in [0.1, 1, 10]:
+            svrLinear = sklearn.svm.SVR(kernel='linear', C=i, max_iter=10000)
+            cvals = sklearn.model_selection.cross_val_score(svrLinear, preppedFeatures, preppedLabels)
+            cvalAvg = np.mean(cvals)
+            if cvalAvg > bestCvalAvg:
+                bestCvalAvg = cvalAvg
+                svrLinear.fit(preppedFeatures, preppedLabels)
+                bestModel = svrLinear
+                bestCvals = cvals
+                bestModelName = "Linear SVR"
+            if np.mean(cvals) > bestCurCvalAvg:
+                bestCurCvals = cvals
+                bestCurCvalAvg = np.mean(cvals)
+        print("Cross Val Scores: " + str(bestCurCvals) + "\nAverage: " + str(bestCurCvalAvg) + "\n")
+        
+        bestCurCvalAvg = 0
+        
+        print("Attempting RBF SVR...")
+        for i in [0.1, 1, 10]:
+            svrRbf = sklearn.svm.SVR(kernel='rbf', C=i, max_iter=10000)
+            cvals = sklearn.model_selection.cross_val_score(svrRbf, preppedFeatures, preppedLabels)
+            cvalAvg = np.mean(cvals)
+            if cvalAvg > bestCvalAvg:
+                bestCvalAvg = cvalAvg
+                svrRbf.fit(preppedFeatures, preppedLabels)
+                bestModel = svrRbf
+                bestCvals = cvals
+                bestModelName = "RBF SVR"
+            if np.mean(cvals) > bestCurCvalAvg:
+                bestCurCvals = cvals
+                bestCurCvalAvg = np.mean(cvals)
+        print("Cross Val Scores: " + str(bestCurCvals) + "\nAverage: " + str(bestCurCvalAvg) + "\n")
+    
+    else: #Classification
+
+        print("\nBeginning model selection.\n")
+        
+        print("Attempting Linear SVC classifier...")
+        bestCurCvalAvg = 0
+        bestModelName = "Linear SVC"
+        for i in [0.1, 1, 10]:
+            linearSVC = sklearn.svm.LinearSVC(C=i)
+            cvals = sklearn.model_selection.cross_val_score(linearSVC, preppedFeatures, preppedLabels)
+            cvalAvg = np.mean(cvals)
+            if cvalAvg > bestCvalAvg:
+                bestCvalAvg = cvalAvg
+                linearSVC.fit(preppedFeatures, preppedLabels)
+                bestModel = linearSVC
+                bestCvals = cvals
+        if np.mean(cvals) > bestCurCvalAvg:
+            bestCurCvals = cvals
+            bestCurCvalAvg = np.mean(cvals)
+        print("Cross Val Scores: " + str(bestCurCvals) + "\nAverage: " + str(bestCurCvalAvg) + "\n")
+        
+        bestCurCvalAvg = 0
+
+        print("Attempting KNN classifier...")
+        for i in [4, 8, 16]:
+            knn = sklearn.neighbors.KNeighborsClassifier(n_neighbors=i)
+            cvals = sklearn.model_selection.cross_val_score(knn, preppedFeatures, preppedLabels)
+            cvalAvg = np.mean(cvals)
+            if cvalAvg > bestCvalAvg:
+                bestCvalAvg = cvalAvg
+                knn.fit(preppedFeatures, preppedLabels)
+                bestModel = knn
+                bestCvals = cvals
+                bestModelName = "KNN"
+            if np.mean(cvals) > bestCurCvalAvg:
+                bestCurCvals = cvals
+                bestCurCvalAvg = np.mean(cvals)
+        print("Cross Val Scores: " + str(bestCurCvals) + "\nAverage: " + str(bestCurCvalAvg) + "\n")
+        
+        bestCurCvalAvg = 0
+        
+        print("Attempting SVC classifier...")
+        for i in [0.1, 1, 10]:
+            svc = sklearn.svm.SVC(C=i, max_iter=10000)
+            cvals = sklearn.model_selection.cross_val_score(svc, preppedFeatures, preppedLabels)
+            cvalAvg = np.mean(cvals)
+            if cvalAvg > bestCvalAvg:
+                bestCvalAvg = cvalAvg
+                svc.fit(preppedFeatures, preppedLabels)
+                bestModel = svc
+                bestCvals = cvals
+                bestModelName = "SVC Classifier"
+            if np.mean(cvals) > bestCurCvalAvg:
+                bestCurCvals = cvals
+                bestCurCvalAvg = np.mean(cvals)
+        print("Cross Val Scores: " + str(bestCurCvals) + "\nAverage: " + str(bestCurCvalAvg) + "\n")
+        
+    print("Best Model Found: " + str(bestModelName) + "\nCross Val Score: " + str(bestCvalAvg))
+
+    numPreds = len(preppedNullFeatures)
+    originalData: np.ndarray
+    originalTarget: np.ndarray
+    if userInput == "1":
+        originalData, originalTarget = sklearn.datasets.load_iris(return_X_y=True)
+    elif userInput == "2":
+        originalData, originalTarget = sklearn.datasets.load_diabetes(return_X_y=True)
+    elif userInput == "3":
+        originalData, originalTarget = sklearn.datasets.load_digits(return_X_y=True)
+    elif userInput == "4":
+        originalData, originalTarget = sklearn.datasets.load_wine(return_X_y=True)
+    elif userInput == "5":
+        originalData, originalTarget = sklearn.datasets.load_breast_cancer(return_X_y=True)
+    testData = np.ndarray((numPreds, originalData.shape[1]))
+    testTarget = np.ndarray((numPreds))
+    predNum = 0
+    for idx, entry in table.iterrows():
+        if pandas.isna(entry[colName]):
+            testData[predNum] = preppedNullFeatures[predNum]
+            testTarget[predNum] = originalTarget[idx]
+            predNum+=1
+    
+    print("Test Score: " + str(bestModel.score(testData, testTarget)))
+
 def main():
     queryString = ("Please enter command.\n"
                    + "1. Perform SQL query\n"
@@ -494,9 +722,10 @@ def main():
                    + "3. Set up example DB\n"
                    + "4. CSV to table (dates can be finnicky)\n"
                    + "5. Delete data from column\n"
-                   + "6. Exit\n")
+                   + "6. Run full test\n"
+                   + "7. Exit\n")
     userInput = input(queryString)
-    while userInput != "6":
+    while userInput != "7":
         if userInput == "1":
             doSql()
         elif userInput == "2":
@@ -507,6 +736,8 @@ def main():
             csvToTableUser()
         elif userInput == "5":
             deleteFromCol()
+        elif userInput == "6":
+            runTest()
         else:
             print("Command not recognized.\n")
         userInput = input(queryString)
